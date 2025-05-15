adb6429b4fea8e164b280eb190c76e4e62625c5c
Umanga-maker <limbuu063@gmail.com>
2025-05-15 09:41:00 +0545

    Add files via upload

---
+++ membership_operators.py
@@ -0,0 +1,24 @@
+# In operator
+
+var = "TutorialsPoint"
+a = "P"
+b = "tor"
+c = "in"
+d = "To"
+print (a, "in", var, ":", a in var)
+print (b, "in", var, ":", b in var)
+print (c, "in", var, ":", c in var)
+print (d, "in", var, ":", d in var)
+
+
+# Not in operator
+
+var = "TutorialsPoint"
+a = "P"
+b = "tor"
+c = "in"
+d = "To"
+print (a, " in", var, ":", a not in var)
+print (b, " in", var, ":", b not in var)
+print (c, " in", var, ":", c not in var)
+print (d, " in", var, ":", d not in var)
