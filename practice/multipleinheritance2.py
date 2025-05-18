475c911e71ec6d036c7f7d1addd40f2be8178741
Umanga-maker <limbuu063@gmail.com>
2025-05-18 13:02:31 +0545

    Add files via upload

---
+++ practice/multipleinheritance2.py
@@ -0,0 +1,27 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def greet (self):
+        return f"Hello, my name is {self.name}."
+
+class Employee:
+    def __init__(self, employee_id):
+        self.employee_id = employee_id
+    def get_id(self):
+        return f"My employee ID is {self.employee_id}."
+class Manager(Person, Employee):
+    def __init__(self, name, employee_id, department):
+        Person.__init__(self, name)
+        Employee.__init__(self, employee_id)
+        self.department = department
+    def show_details(self):
+        return f"{self.greet()} {self.get_id()} I m a Manager in the {self.department} department."
+
+manager = Manager("Alice", "E12345", "Sales")
+
+print(manager.greet())
+print(manager.get_id())
+print(manager.show_details())
+        
+
+
