7ce83e1e352e60b01ff012876bbcba722d602952
Umanga-maker <limbuu063@gmail.com>
2025-05-18 12:53:02 +0545

    Add files via upload

---
+++ practice/Operators.py
@@ -0,0 +1,25 @@
+x = 10
+print(x)
+
+# x += 10
+# print(x)# Equivalent to x = x + 10
+
+# x -=3
+# print(x)# Equivalent to x = x - 3
+
+# x *= 2
+# print(x)# Equivalent to x = x * 2
+
+# x /= 2
+# print(x)# Equivalent to x = x / 2
+
+# x //= 3
+# print(x)#floor division discards the decimal part of the result and returns the integer part. 
+
+# x %= 3
+# print(x)#gives the remainder # x is now 1 (10 % 3=1)
+
+x **= 2
+print(x)#goes in the power 
+
+
